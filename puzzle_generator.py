4cac087af52a9eca3d7e52b7917d5cf581a13f6a
ljdross <91616509+ljdross@users.noreply.github.com>
2021-09-30 01:19:59 +0200

    rewritten for blender 2.93

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -4,9 +4,9 @@ from math import radians
 # delete everything
 bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete(use_global=True)
-bpy.context.scene.cursor_location[0] = 0
-bpy.context.scene.cursor_location[1] = 0
-bpy.context.scene.cursor_location[2] = 0
+bpy.context.scene.cursor.location[0] = 0
+bpy.context.scene.cursor.location[1] = 0
+bpy.context.scene.cursor.location[2] = 0
 
 
 # create first cube
@@ -30,7 +30,7 @@ visual_cube2.parent = visual_cube1
 
 # select first object
 bpy.ops.object.select_all(action='DESELECT')
-bpy.data.objects['visual_cube1'].select = True
+bpy.data.objects['visual_cube1'].select_set(True)
 #bpy.context.scene.objects.active = visual_cube1
 
 # create link at origin
@@ -38,20 +38,19 @@ bpy.ops.phobos.create_links(location='selected objects', size=8, parent_link=Tru
 
 # select
 bpy.ops.object.select_all(action='DESELECT')
-bpy.data.objects['visual_cube2'].select = True
+bpy.data.objects['visual_cube2'].select_set(True)
 # create link at origin
 bpy.ops.phobos.create_links(location='selected objects', size=8, parent_link=True, parent_objects=True, nameformat="link1")
 
 # define joint constraint at child link
-bpy.ops.phobos.define_joint_constraints(passive=True, useRadian=False, joint_type='revolute', upper=135)
+bpy.ops.phobos.define_joint_constraints(passive=True, useRadian=False, joint_type='revolute', lower=0, upper=90)
 
 
 # export model
-bpy.context.scene.phobosexportsettings.path = "/home/userone/ba/puzzles/p8/"
+bpy.context.scene.phobosexportsettings.path = "/home/userone/ba/puzzle-generator/puzzles/simple_two_cubes"
 bpy.context.scene.phobosexportsettings.selectedOnly = False
 bpy.context.scene.export_entity_urdf = True
 bpy.ops.phobos.name_model(modelname="simple_two_cubes")
 bpy.ops.phobos.export_model(modelname='simple_two_cubes')
 
-# test
 
