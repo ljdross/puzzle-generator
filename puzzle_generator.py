06251f46079bc9fe4146eb15eeacb05e37e624d9
ljdross <91616509+ljdross@users.noreply.github.com>
2021-10-05 20:47:24 +0200

    create collision objects, rotation and scale params for create_cube(), reset cursor rotation

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -2,8 +2,8 @@ import bpy
 from math import radians
 
 
-def create_cube(name="cube", position=(0, 0, 0)):
-	bpy.ops.mesh.primitive_cube_add(location=position)
+def create_cube(name="cube", location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)):
+	bpy.ops.mesh.primitive_cube_add(location=location, rotation=rotation, scale=scale)
 	cube = bpy.context.active_object
 	bpy.ops.phobos.set_phobostype(phobostype='visual')
 	bpy.ops.phobos.define_geometry(geomType='box')
@@ -15,17 +15,16 @@ def create_cube(name="cube", position=(0, 0, 0)):
 # delete everything
 bpy.ops.object.select_all(action='SELECT')
 bpy.ops.object.delete(use_global=True)
-bpy.context.scene.cursor.location[0] = 0
-bpy.context.scene.cursor.location[1] = 0
-bpy.context.scene.cursor.location[2] = 0
+bpy.context.scene.cursor.location = (0, 0, 0)
+bpy.context.scene.cursor.rotation_euler = (0, 0, 0)
 
 
 # create first cube
-visual_cube1 = create_cube("visual_cube1", (0, 0, 0))
+visual_cube1 = create_cube(name="visual_cube1", location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1))
 
 
 # create second cube
-visual_cube2 = create_cube("visual_cube2", (0, 0, 2))
+visual_cube2 = create_cube(name="visual_cube2", location=(0, 0, 2), rotation=(0, 0, radians(45)), scale=(1, 2, 1))
 
 
 # parent-child relationship
@@ -48,6 +47,10 @@ bpy.ops.phobos.create_links(location='selected objects', size=8, parent_link=Tru
 
 # define joint constraint at child link
 bpy.ops.phobos.define_joint_constraints(passive=True, useRadian=False, joint_type='revolute', lower=0, upper=90)
+
+# create collision objects from visual objects
+bpy.ops.phobos.select_model()
+bpy.ops.phobos.create_collision_objects()
 
 
 # export model
