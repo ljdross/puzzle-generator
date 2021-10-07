21970356c8dcd034bbffa409bb03ee9afeabdc07
ljdross <91616509+ljdross@users.noreply.github.com>
2021-10-07 02:47:32 +0200

    more structure through functions, implemented sliders

--- puzzle_generator.py
+++ puzzle_generator.py
@@ -1,62 +1,67 @@
 import bpy
 from math import radians
 
+dir_for_output = "/home/userone/ba/puzzle-generator/puzzles/simple_sliders"
+modelname = "simple_sliders"
+number_of_sliders = 5
 
-def create_cube(name="cube", location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)):
+
+def reset():
+	bpy.ops.object.select_all(action='SELECT')
+	bpy.ops.object.delete(use_global=True)
+	bpy.context.scene.cursor.location = (0, 0, 0)
+	bpy.context.scene.cursor.rotation_euler = (0, 0, 0)
+
+def create_cube(name, parent=None, location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1)):
 	bpy.ops.mesh.primitive_cube_add(location=location, rotation=rotation, scale=scale)
 	cube = bpy.context.active_object
 	bpy.ops.phobos.set_phobostype(phobostype='visual')
 	bpy.ops.phobos.define_geometry(geomType='box')
 	cube.name = name
+	if parent is not None:
+		cube.parent = parent
 	return cube
+
+def create_link_and_joint(obj, name, joint_type=None, lower=0, upper=0):
+	bpy.ops.object.select_all(action='DESELECT')
+	obj.select_set(True)
+	bpy.ops.phobos.create_links(location='selected objects', size=8, parent_link=True, parent_objects=True, nameformat=name)
+	if joint_type is not None:
+		bpy.ops.phobos.define_joint_constraints(passive=True, joint_type=joint_type, lower=lower, upper=upper)
+
+def create_collision():
+	bpy.ops.phobos.select_model()
+	bpy.ops.phobos.create_collision_objects()
+
+def export(directory, filename):
+	bpy.context.scene.phobosexportsettings.path = directory
+	bpy.context.scene.phobosexportsettings.selectedOnly = False
+	bpy.context.scene.export_entity_urdf = True
+	bpy.ops.phobos.name_model(modelname=filename)
+	bpy.ops.phobos.export_model()
 
 
 
 # delete everything
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.object.delete(use_global=True)
-bpy.context.scene.cursor.location = (0, 0, 0)
-bpy.context.scene.cursor.rotation_euler = (0, 0, 0)
+reset()
 
+# create floor
+floor = create_cube(name="floor", location=(0, 0, -0.1), scale=(8, 8, 0.1))
+# create sliders
+sliders = []
+for i in range(number_of_sliders):
+	if i % 2 == 0:
+		sliders.append(create_cube(name="visual_cube" + str(i), parent=floor, location=((i/2), (i/-2), 0.1), rotation=(radians(90), 0, 0), scale=(0.1, 0.1, 0.8)))
+	else:
+		sliders.append(create_cube(name="visual_cube" + str(i), parent=floor, location=(((i-1)/2), (((i-1)/-2)-1), 0.1), rotation=(0, radians(90), 0), scale=(0.1, 0.1, 0.8)))
 
-# create first cube
-visual_cube1 = create_cube(name="visual_cube1", location=(0, 0, 0), rotation=(0, 0, 0), scale=(1, 1, 1))
-
-
-# create second cube
-visual_cube2 = create_cube(name="visual_cube2", location=(0, 0, 2), rotation=(0, 0, radians(45)), scale=(1, 2, 1))
-
-
-# parent-child relationship
-visual_cube2.parent = visual_cube1
-
-
-# select first object
-bpy.ops.object.select_all(action='DESELECT')
-bpy.data.objects['visual_cube1'].select_set(True)
-#bpy.context.scene.objects.active = visual_cube1
-
-# create link at origin
-bpy.ops.phobos.create_links(location='selected objects', size=8, parent_link=True, parent_objects=True, nameformat="base_link")
-
-# select
-bpy.ops.object.select_all(action='DESELECT')
-bpy.data.objects['visual_cube2'].select_set(True)
-# create link at origin
-bpy.ops.phobos.create_links(location='selected objects', size=8, parent_link=True, parent_objects=True, nameformat="link1")
-
-# define joint constraint at child link
-bpy.ops.phobos.define_joint_constraints(passive=True, useRadian=False, joint_type='revolute', lower=0, upper=90)
+# create links (at origins)
+create_link_and_joint(floor, "base_link")
+for i, s in enumerate(sliders):
+	create_link_and_joint(s, "link" + str(i), joint_type='prismatic', upper=2)
 
 # create collision objects from visual objects
-bpy.ops.phobos.select_model()
-bpy.ops.phobos.create_collision_objects()
-
+create_collision()
 
 # export model
-bpy.context.scene.phobosexportsettings.path = "/home/userone/ba/puzzle-generator/puzzles/simple_two_cubes"
-bpy.context.scene.phobosexportsettings.selectedOnly = False
-bpy.context.scene.export_entity_urdf = True
-bpy.ops.phobos.name_model(modelname="simple_two_cubes")
-bpy.ops.phobos.export_model(modelname='simple_two_cubes')
-
+export(dir_for_output, modelname)
