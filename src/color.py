a5e434c5fe31f8d675193e2fe60d809aa74f831d
ljdross <91616509+ljdross@users.noreply.github.com>
2022-01-27 21:05:50 +0100

    refactor (minor): add names and remove redundancies in color module

--- src/color.py
+++ src/color.py
@@ -1,10 +1,13 @@
 import bpy
 
-LAVENDER = bpy.data.materials.new("RGB")
-LAVENDER.diffuse_color = (0.8, 0.8, 1, 1)
-RED = bpy.data.materials.new("RGB")
-RED.diffuse_color = (1, 0, 0, 1)
-GREEN = bpy.data.materials.new("RGB")
-GREEN.diffuse_color = (0, 1, 0, 1)
-WHITE = bpy.data.materials.new("RGB")
-WHITE.diffuse_color = (1, 1, 1, 1)
+
+def new_color_material(rgba_tuple, name="rgba_color"):
+    color = bpy.data.materials.new(name)
+    color.diffuse_color = rgba_tuple
+    return color
+
+
+LAVENDER = new_color_material((0.8, 0.8, 1, 1), "lavender")
+RED = new_color_material((1, 0, 0, 1), "red")
+GREEN = new_color_material((0, 1, 0, 1), "green")
+WHITE = new_color_material((1, 1, 1, 1), "white")
