c76f2277cbe4a2f2d97a0a378b48de5f4c8f7fb1
ljdross <91616509+ljdross@users.noreply.github.com>
2022-02-24 19:23:45 +0100

    feat (minor): add YELLOW

--- src/color.py
+++ src/color.py
@@ -10,5 +10,6 @@ def new_color_material(rgba_tuple, name="rgba_color"):
 LAVENDER = new_color_material((0.8, 0.8, 1, 1), "lavender")
 RED = new_color_material((1, 0, 0, 1), "red")
 GREEN = new_color_material((0, 1, 0, 1), "green")
+YELLOW = new_color_material((1, 1, 0, 1), "yellow")
 WHITE = new_color_material((1, 1, 1, 1), "white")
 GRAY = new_color_material((0.5, 0.5, 0.5, 1), "gray")
