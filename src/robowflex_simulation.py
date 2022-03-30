efb0e29545f1c919950ce7ca57720c47de4fdb33
ljdross <91616509+ljdross@users.noreply.github.com>
2022-03-30 21:49:21 +0200

    feat: benchmarking with robowflex

--- src/robowflex_simulation.py
+++ src/robowflex_simulation.py
@@ -18,8 +18,8 @@ def adjust_absolute_filepaths(robowflex_workspace="../rb_ws"):
     run(["python3", "filepath_organizer.py"], cwd=robowflex_workspace + "/src/robowflex/robowflex_dart/include/io")
 
 
-def solve(urdf_path="puzzles/simple_sliders/urdf/simple_sliders.urdf", planning_time=5,
-          robowflex_workspace="../rb_ws", adjust_filepaths=False):
+def solve(urdf_path="puzzles/simple_sliders/urdf/simple_sliders.urdf", planning_time=5, adjust_filepaths=False,
+          benchmark_runs=0, robowflex_workspace="../rb_ws"):
     puzzle_directory = path.dirname(path.dirname(urdf_path))
     puzzle_name = puzzle_directory.split('/')[-1]
 
@@ -27,17 +27,25 @@ def solve(urdf_path="puzzles/simple_sliders/urdf/simple_sliders.urdf", planning_
     create_srdf(robowflex_workspace + "/src/robowflex/robowflex_dart/include/io/envs/" + puzzle_name + "/urdf/"
                 + puzzle_name + ".urdf")
 
-    # TODO: support URDFs with meshes
     if adjust_filepaths:
         adjust_absolute_filepaths(robowflex_workspace)
 
-    # TODO: catch errors in urdf2config.py e.g. no srdf
-    run(["./solve_puzzle", puzzle_name, str(planning_time)], cwd=robowflex_workspace + "/devel/lib/robowflex_dart/")
+    if not benchmark_runs:
+        # TODO: support URDFs with meshes
+        # TODO: catch errors in urdf2config.py e.g. no srdf
+        run(["./solve_puzzle", puzzle_name, str(planning_time)], cwd=robowflex_workspace + "/devel/lib/robowflex_dart/")
 
-    if stat(robowflex_workspace + "/src/robowflex/robowflex_dart/include/io/path_result/" + puzzle_name +
-            ".txt").st_size == 0:
-        print("NO SOLUTION FOUND!")
-        return 1
+        if stat(robowflex_workspace + "/src/robowflex/robowflex_dart/include/io/path_result/" + puzzle_name
+                + ".txt").st_size == 0:
+            print("NO SOLUTION FOUND!")
+            return 1
+        else:
+            print("FOUND SOLUTION!")
+            return 0
+
     else:
-        print("FOUND SOLUTION!")
-        return 0
+        run(["./benchmark_main", puzzle_name, str(planning_time), str(benchmark_runs)],
+            cwd=robowflex_workspace + "/devel/lib/robowflex_dart/")
+        print("benchmark results(db):")
+        print("file://" + path.abspath(robowflex_workspace + "/src/robowflex/robowflex_dart/include/io/db_files/"
+                                       + puzzle_name + ".db"))
