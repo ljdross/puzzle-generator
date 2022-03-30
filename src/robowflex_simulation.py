6c9c7c8973ac3eac828c0ed1b297b6b9e84fcb53
ljdross <91616509+ljdross@users.noreply.github.com>
2022-03-30 09:55:55 +0200

    feat (minor): add param adjust_filepaths to test_solvability() in robowflex_simulation.py

--- src/robowflex_simulation.py
+++ src/robowflex_simulation.py
@@ -19,17 +19,21 @@ def adjust_absolute_filepaths(robowflex_workspace="../rb_ws"):
 
 
 def test_solvability(urdf_path="puzzles/simple_sliders/urdf/simple_sliders.urdf", planning_time=5,
-                     robowflex_workspace="../rb_ws"):
+                     robowflex_workspace="../rb_ws", adjust_filepaths=False):
     puzzle_directory = path.dirname(path.dirname(urdf_path))
     puzzle_name = puzzle_directory.split('/')[-1]
 
     copy_puzzle(puzzle_directory, robowflex_workspace)
     create_srdf(robowflex_workspace + "/src/robowflex/robowflex_dart/include/io/envs/" + puzzle_name + "/urdf/"
                 + puzzle_name + ".urdf")
+
     # TODO: support URDFs with meshes
-    # adjust_absolute_filepaths(robowflex_workspace)
-    run(["./solve_puzzle", puzzle_name, str(planning_time)], cwd=robowflex_workspace + "/devel/lib/robowflex_dart/")
+    if adjust_filepaths:
+        adjust_absolute_filepaths(robowflex_workspace)
+
     # TODO: catch errors in urdf2config.py e.g. no srdf
+    run(["./solve_puzzle", puzzle_name, str(planning_time)], cwd=robowflex_workspace + "/devel/lib/robowflex_dart/")
+
     if stat(robowflex_workspace + "/src/robowflex/robowflex_dart/include/io/path_result/" + puzzle_name +
             ".txt").st_size == 0:
         print("NO SOLUTION FOUND!")
