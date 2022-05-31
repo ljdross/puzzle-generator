2a32acf9ac9321353c9f7a5eddcf9eb17caedf54
ljdross <91616509+ljdross@users.noreply.github.com>
2022-05-31 23:41:34 +0200

    feat (minor): add dict DIRECTION_VECTOR_2D

--- src/calc.py
+++ src/calc.py
@@ -6,6 +6,13 @@ RAD90 = round(radians(90), 5)
 RAD180 = round(radians(180), 5)
 RAD360 = round(radians(360), 5)
 
+DIRECTION_VECTOR_2D = {
+    "N": (0, 1),
+    "E": (1, 0),
+    "S": (0, -1),
+    "W": (-1, 0),
+}
+
 
 def tuple_add(tuple_a: tuple, tuple_b: tuple) -> tuple:
     """Return the result of the addition of two tuples."""
